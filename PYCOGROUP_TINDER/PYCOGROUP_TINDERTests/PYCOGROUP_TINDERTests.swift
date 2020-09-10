4554e01fcee37fc543e577d6793967d0de060f0d
Ngo Van Tu Cuong <cuongnvt@vnext.com.vn>
2020-09-10 19:43:24 +0700

    Init source

---
+++ PYCOGROUP_TINDER/PYCOGROUP_TINDERTests/PYCOGROUP_TINDERTests.swift
@@ -0,0 +1,34 @@
+//
+//  PYCOGROUP_TINDERTests.swift
+//  PYCOGROUP_TINDERTests
+//
+//  Created by ngovantucuong on 9/10/20.
+//  Copyright © 2020 ngovantucuong. All rights reserved.
+//
+
+import XCTest
+@testable import PYCOGROUP_TINDER
+
+class PYCOGROUP_TINDERTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
