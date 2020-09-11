ded10a377275aaf3cb06e8594f6266ebe21e036e
Ngo Van Tu Cuong <cuongnvt@vnext.com.vn>
2020-09-11 22:02:27 +0700

    - Add list view action button

--- PYCOGROUP_TINDER/PYCOGROUP_TINDER/Common/Views/CardView.swift
+++ PYCOGROUP_TINDER/PYCOGROUP_TINDER/Common/Views/CardView.swift
@@ -19,10 +19,21 @@ class CardView: UIView {
         return img
     }()
     
+    private let stackViewActionButton: UIStackView = {
+       let st = UIStackView()
+        st.translatesAutoresizingMaskIntoConstraints = false
+        st.axis = .horizontal
+        st.alignment = .fill
+        st.distribution = .fillEqually
+        st.spacing = 0
+        return st
+    }()
+    
     // MARK: - Init
     override init(frame: CGRect) {
         super.init(frame: frame)
         addSubCardView()
+        addListActionButton()
     }
     
     required init?(coder: NSCoder) {
@@ -44,11 +55,58 @@ class CardView: UIView {
         addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(handlePanGesture(gesture:))))
     }
     
+    private func addViewAction(index: Int) {
+       let parentView = UIView()
+       parentView.translatesAutoresizingMaskIntoConstraints = false
+       parentView.tag = index
+       parentView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTapButtonAction(sender:))))
+       
+       let seperatorView = UIView()
+       seperatorView.translatesAutoresizingMaskIntoConstraints = false
+       seperatorView.backgroundColor = .blue
+       parentView.addSubview(seperatorView)
+       
+       let imageView = UIImageView()
+       imageView.translatesAutoresizingMaskIntoConstraints = false
+       imageView.image = UIImage(named: "user1")
+       imageView.contentMode = .scaleAspectFit
+       parentView.addSubview(imageView)
+       // Contraint UI
+       NSLayoutConstraint.activate([
+           seperatorView.topAnchor.constraint(equalTo: parentView.topAnchor),
+           seperatorView.leadingAnchor.constraint(equalTo: parentView.leadingAnchor),
+           seperatorView.trailingAnchor.constraint(equalTo: parentView.trailingAnchor),
+           seperatorView.heightAnchor.constraint(equalToConstant: 3.0)
+       ])
+       NSLayoutConstraint.activate([
+           imageView.topAnchor.constraint(equalTo: seperatorView.bottomAnchor, constant: 0),
+           imageView.leadingAnchor.constraint(equalTo: parentView.leadingAnchor),
+           imageView.trailingAnchor.constraint(equalTo: parentView.trailingAnchor),
+           imageView.bottomAnchor.constraint(equalTo: parentView.bottomAnchor)
+       ])
+       stackViewActionButton.addArrangedSubview(parentView)
+   }
+    
+    private func addListActionButton() {
+        addSubview(stackViewActionButton)
+        stackViewActionButton.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -20).isActive = true
+        stackViewActionButton.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 10).isActive = true
+        stackViewActionButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -10).isActive = true
+        stackViewActionButton.heightAnchor.constraint(equalToConstant: 60).isActive = true
+        for i in 0...3 {
+            addViewAction(index: i)
+        }
+    }
+    
     // MARK: - Func objc
+    @objc func handleTapButtonAction(sender: UITapGestureRecognizer) {
+        print("in ra", sender.view?.tag)
+    }
+    
     @objc func handlePanGesture(gesture: UIPanGestureRecognizer) {
         let translation = gesture.translation(in: self)
         
-        let LIMIT_VALUE_MOVE: Int = 100
+        let LIMIT_VALUE_MOVE: Int = 120
         switch gesture.state {
         case .changed:
             let valueMove = abs(translation.x)
